bc37565de12383463744220c98feacf55abbdf44
Alger Brigham <alger.brigham@cayusetechnologies.com>
2018-09-05 13:24:29 -0700

    Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,71 @@
+// Dom7
+var $$ = Dom7;
+
+// install plugin first
+Framework7.use(debugPlugin);
+
+// Framework7 App main instance
+var app = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+  // Enable panel left visibility breakpoint
+  panel: {
+    leftBreakpoint: 960,
+  },
+});
+
+// Init/Create left panel view
+var mainView = app.views.create('.view-left', {
+  url: '/'
+});
+
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+  url: '/'
+});
+
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
+
+$$('.convert-form-to-data').on('click', function () {
+  var formData = app.form.convertToData('#my-form');
+  alert(JSON.stringify(formData));
+});
+
+$$('.fill-form-from-data').on('click', function () {
+  var formData = {
+    'name': 'John',
+    'email': 'john@doe.com',
+    'gender': 'female',
+    'toggle': ['yes'],
+  }
+  app.form.fillFromData('#my-form', formData);
+});
